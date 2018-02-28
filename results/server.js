4ae0eb2d3e91a01d762b79fca57cf566dd435c03
iDev247 <idev247@gmail.com>
2018-02-28 16:17:12 -0500

    Update server.js

--- results/server.js
+++ results/server.js
@@ -55,6 +55,5 @@ app.get('/', function (req, res) {
 });
 
 server.listen(port, function () {
-  var port = server.address().port;
-  console.log('App running on port ' + port);
+  console.log('App running on port ' + server.address().port);
 });
